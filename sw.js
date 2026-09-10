2e7da3c04c1af3c51be9482bf7d46a9d701c9d5a
Tomonori-Oishi <Tomonori-Oishi@users.noreply.github.com>
2026-09-10 10:30:14 +0900

    Rename app to WANDERIA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'my-world-v31';
+const CACHE = 'my-world-v32';
 const ASSETS = ['./', './index.html', './style.css', './figma-footer.css', './app.js', './manifest.webmanifest', './compass.svg', './figma-assets/generate.svg', './figma-assets/map.svg', './figma-assets/memories.svg', './figma-assets/history.svg', './assets-world-bright.png'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener('fetch', (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
